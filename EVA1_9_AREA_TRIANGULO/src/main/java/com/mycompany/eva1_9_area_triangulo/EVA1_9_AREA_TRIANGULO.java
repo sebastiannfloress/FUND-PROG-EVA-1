3e7a50506c9731468b8198a341e59a765ab503a8
sebastiannfloress <143568236+sebastiannfloress@users.noreply.github.com>
2023-09-12 18:41:52 -0700

    Add files via upload

---
+++ EVA1_9_AREA_TRIANGULO/src/main/java/com/mycompany/eva1_9_area_triangulo/EVA1_9_AREA_TRIANGULO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_area_triangulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saulf
+ */
+public class EVA1_9_AREA_TRIANGULO {
+
+    public static void main(String[] args) {
+      double base;
+      double altura;
+      double area;
+      Scanner input = new Scanner(System.in);
+      
+      System.out.println("Introduce la base del triangulo:");
+      base = input.nextDouble();
+      System.out.println("Introduce la altura del triangulo:");
+      altura = input.nextDouble();
+      area = (base * altura) / 2;
+      System.out.println("El area del triangulo es:");
+      System.out.println(area);
+      
+      
+
+      
+            
+        
+     
+        
+        
+        
+        
+    }
+}
